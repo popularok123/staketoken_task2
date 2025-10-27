f32e3c5e792889eb0113281695388e74a7edcaba
popularok123 <popularok123@gmail.com>
2025-10-27 16:10:34 +0800

    iii

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -11,7 +11,13 @@ const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY as string
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
   networks: {
-    hardhat: {},
+    hardhat: {
+      chainId: 31337,
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      chainId: 31337,
+    },
     sepolia: {
       url:
         "https://eth-sepolia.g.alchemy.com/v2/" + ALCHEMY_API_KEY,
